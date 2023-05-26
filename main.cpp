6b00b440acc67bc0488a8cd835287f38c60ab28b
Dylan Landry <dylan.landry@milkmoovement.com>
2023-05-26 12:45:03 -0400

    add BinaryTreeNode

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
+// It's binary because each node has at most 2 children.
+class BinaryTreeNode {
+public:
+	int value;
+	BinaryTreeNode* child1;
+	BinaryTreeNode* child2;
+
+	BinaryTreeNode(int _value) {
+		value = _value;
+		child1 = nullptr;
+		child2 = nullptr;
+	}
+};
+
 int main() {
-	std::cout << "Hello World!";
+	BinaryTreeNode parent(1);
+	BinaryTreeNode child1(2);
+	BinaryTreeNode child2(3);
+
+	parent.child1 = &child1;
+	parent.child2 = &child2;
+
+	std::cout << "parent: " << parent.value << '\n'; 
+	std::cout << "child 1: " << parent.child1->value << '\n'; 
+	std::cout << "child 2: " << parent.child2->value << '\n'; 
+
 	return 0;
 }
--- readme.md
+++ readme.md
@@ -5,3 +5,11 @@ I want to implement some kind of tree structure using cpp.
 You can use the shell script `run.sh` to both compile and run the project.
 
 `./run.sh`
+
+# About Trees
+
+https://en.wikipedia.org/wiki/Tree_(data_structure)
+
+# Future Plans
+
+Might be cool to somehow export this tree & make a renderer with TS, a tree-viewer. That might be a way I'd be excited to use TS and React.
